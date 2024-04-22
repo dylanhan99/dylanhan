e22841074a6b1c8aa40ce7d1c695386ec3e21124
dylanhan99 <dylanhan99@gmail.com>
2024-04-22 13:49:42 +0800

    Initial commit + boilerplate navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavbarComponent } from './navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    
+    NavbarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'dylanhan';
+  title = 'Dylan Han';
 }
